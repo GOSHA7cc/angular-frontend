05ddf6ad8b8da1b53e433630366ca50d4726b926
William Cavalcante <williamdemelo2017@gmail.com>
2023-11-27 02:21:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component'
 
 @Component({
   selector: 'app-root',
